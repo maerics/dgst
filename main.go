33ba66b4bfedd0fa7d92646ba5a1f87891186575
Michael Ericson <maerics@gmail.com>
2022-01-31 16:46:43 -0700

    Add "key" flag for blake2 family

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 	"golang.org/x/crypto/blake2b"
@@ -23,6 +24,8 @@ import (
 type Options struct {
 	Base64 bool
 	Binary bool
+
+	Key string
 }
 
 func main() {
@@ -43,6 +46,10 @@ func main() {
 			"print hash values encoded as base64")
 		cmd.Flags().BoolVarP(&options.Binary, "binary", "b", options.Binary,
 			"print hash values directly without encoding")
+		if strings.HasPrefix(name, "blake2") {
+			cmd.Flags().StringVarP(&options.Key, "key", "k", options.Key,
+				"hex encoded key for use with blake2 family of size 0-64 bytes")
+		}
 		rootCmd.AddCommand(cmd)
 	}
 
@@ -56,29 +63,29 @@ func main() {
 	}
 }
 
-var hashes = map[string]func() hash.Hash{
-	"md4": md4.New,
-	"md5": md5.New,
+var hashes = map[string]func(*Options) hash.Hash{
+	"md4": func(*Options) hash.Hash { return md4.New() },
+	"md5": func(*Options) hash.Hash { return md5.New() },
 
-	"sha1":     sha1.New,
-	"sha256":   sha256.New,
-	"sha384":   sha512.New384,
-	"sha512":   sha512.New,
-	"sha3-224": sha3.New224,
-	"sha3-256": sha3.New256,
-	"sha3-384": sha3.New384,
-	"sha3-512": sha3.New512,
+	"sha1":     func(*Options) hash.Hash { return sha1.New() },
+	"sha256":   func(*Options) hash.Hash { return sha256.New() },
+	"sha384":   func(*Options) hash.Hash { return sha512.New384() },
+	"sha512":   func(*Options) hash.Hash { return sha512.New() },
+	"sha3-224": func(*Options) hash.Hash { return sha3.New224() },
+	"sha3-256": func(*Options) hash.Hash { return sha3.New256() },
+	"sha3-384": func(*Options) hash.Hash { return sha3.New384() },
+	"sha3-512": func(*Options) hash.Hash { return sha3.New512() },
 
-	"blake2-256": blakeEmptyKey(blake2b.New256),
-	"blake2-384": blakeEmptyKey(blake2b.New384),
-	"blake2-512": blakeEmptyKey(blake2b.New512),
+	"blake2-256": blakeKey(blake2b.New256),
+	"blake2-384": blakeKey(blake2b.New384),
+	"blake2-512": blakeKey(blake2b.New512),
 
-	"ripemd160": ripemd160.New,
+	"ripemd160": func(*Options) hash.Hash { return ripemd160.New() },
 }
 
-func printHash(name string, hfn func() hash.Hash, o *Options) func(*cobra.Command, []string) {
+func printHash(name string, hfn func(*Options) hash.Hash, o *Options) func(*cobra.Command, []string) {
 	return func(c *cobra.Command, s []string) {
-		h := hfn()
+		h := hfn(o)
 		if _, err := io.Copy(h, os.Stdin); err != nil {
 			log.Fatalf("FATAL: failed to compute %q hash from stdin: %v", name, err)
 		}
@@ -99,10 +106,23 @@ func printHash(name string, hfn func() hash.Hash, o *Options) func(*cobra.Comman
 	}
 }
 
-func blakeEmptyKey(f func([]byte) (hash.Hash, error)) func() hash.Hash {
-	h, err := f(nil)
-	if err != nil {
-		panic(err)
+func blakeKey(f func([]byte) (hash.Hash, error)) func(*Options) hash.Hash {
+	return func(o *Options) hash.Hash {
+		var key []byte = nil
+		if o.Key != "" {
+			if k, err := hex.DecodeString(o.Key); err != nil {
+				log.Fatalf("FATAL: invalid hex encoded key: %v.", err)
+			} else if len(k) > 64 {
+				log.Fatalf("FATAL: key is too long, got %v byte, wanted <= 64.", len(k))
+			} else {
+				key = k
+			}
+		}
+
+		h, err := f(key)
+		if err != nil {
+			log.Fatalf("FATAL: failed to create new blake2 key: %v", err)
+		}
+		return h
 	}
-	return func() hash.Hash { return h }
 }
